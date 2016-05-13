965662ac27b079e4c2699dd01ef9e55d9faad904
MattiaLodi <lodi1997@live.it>
2016-05-13 08:47:31 +0200

    i0dbk

--- views/home.php
+++ views/home.php
@@ -20,16 +20,33 @@
 	.content {
 		padding: 24px;
 	}
+	div.title {
+        color:white;
+        padding:24px;
+        font-weight: 300;
+    }
+    div.row {
+        margin-bottom: 0!important;
+    }
+    a {
+        color:white;
+        font-size: 24px;
+        letter-spacing: 0.5px;
+    }
 </style>
 <?php require_once 'secondo.php'; ?>
 <main>
-
 	<div class="header">
 		<div class="content" style="background-color: black">
 			<img src="/img/elephante.png">
 			<!-- <p style="margin:0">THE PLACE TO BE FOR 3D</p> -->
 			<h3>What is DDDParts?</h3>
 			<p style="width: 700px; margin: auto">DDDParts is a social network where users can upload and share their DDDProjects or download one for print its!</p>
+		</div>
+		<div class="row title">
+            <a href="/" class="accent-2 white-text center waves-effect waves-light btn-flat" style="background-color: #757575">Try Now!</a>
+        </div>
+			
 		</div>
 	</div>
 </main>
