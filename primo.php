3c7515c6a469ba988d1362700304c587139593f8
eugeniogrigoras <eugeniogrigoras@gmail.com>
2016-05-19 11:23:59 +0200

    sdds

--- primo.php
+++ primo.php
@@ -9,6 +9,7 @@
         <meta name="keywords" content="3D 3DParts DDDParts printer stampante parti social profile sharing 3d printer 3d printer for sale 3d printer pen 3d printer reviews 3d printer filament 3d printer price 3d printer kit 3d printer designs 3d printer amazon 3d printer software">
         <meta name="theme-color" content="#424242">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" href="/css/normalize.css">
