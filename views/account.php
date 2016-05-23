7261eed1dc6ed80ae6d54d7cf9ff9cc067d3af04
eugeniogrigoras <eugeniogrigoras@gmail.com>
2016-05-23 11:39:55 +0200

    sad

--- views/account.php
+++ views/account.php
@@ -23,8 +23,8 @@
         margin-bottom: 0;
     }
     #avatar img {
-        width: 150px;
-        height: 150px;
+        width: 100%;
+        height: 100%;
     }
     #avatar {
         overflow: hidden;
@@ -32,6 +32,9 @@
         
         width: 150px;
         height: 150px;
+        background-size: cover; 
+        z-index: 2;
+        text-align: center;
         margin:auto auto;
     }
 
@@ -117,15 +120,6 @@
 
     a {
         color: rgba(255,109,64,1);
-    }
-
-    .card-image {
-    position: relative;
-    height: 150px;
-    background-position: 50% 50%;
-    background-repeat:   no-repeat;
-    background-size:     cover;
-
     }
 
 </style>
